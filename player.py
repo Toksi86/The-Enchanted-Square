ed2d6730c25d69493480fb1c8ecb7c6d0e4dc277
Toksi86 <puma-yellow@yandex.ru>
2024-01-09 19:18:47 +0500

    Исправлен неверный путь к файлу с изначальным изображением персонажа

--- player.py
+++ player.py
@@ -6,7 +6,7 @@ from support import import_folder
 class Player(pygame.sprite.Sprite):
     def __init__(self, pos, groups, obstacle_sprites):
         super().__init__(groups)
-        self.image = pygame.image.load('sprites/player/player.png').convert_alpha()
+        self.image = pygame.image.load('sprites/player/up/player_up.png').convert_alpha()
         self.rect = self.image.get_rect(topleft=pos)
         self.hitbox = self.rect.inflate(0, -10)
 
