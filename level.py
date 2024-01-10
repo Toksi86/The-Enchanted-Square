63998ade30293db1d2fb9267c64a4ac27e15b624
Toksi86 <puma-yellow@yandex.ru>
2024-01-10 19:16:39 +0500

    Добавлен UI: ХП и Мана показатели, количество опыта, активное оружие

--- level.py
+++ level.py
@@ -5,6 +5,7 @@ from settings import *
 from support import *
 from tile import Tile
 from weapon import Weapon
+from ui import UI
 
 
 class Level:
@@ -21,6 +22,9 @@ class Level:
 
         # sprite setup
         self.create_map()
+
+        # user interface
+        self.ui = UI()
 
     def create_attack(self):
         self.current_attack = Weapon(self.player, [self.visible_sprites])
@@ -61,6 +65,7 @@ class Level:
         # update and draw the game
         self.visible_sprites.custom_draw(self.player)
         self.visible_sprites.update()
+        self.ui.display(self.player)
 
 
 class YSortCameraGroup(pygame.sprite.Group):
--- settings.py
+++ settings.py
@@ -3,8 +3,28 @@ HEIGHT = 720
 FPS = 60
 TILESIZE = 64
 
+# ui
+BAR_HEIGHT = 20
+HEALTH_BAR_WIDTH = 200
+ENERGY_BAR_WIDTH = 150
+ITEM_BOX_SIZE = 80
+# TODO: download font and add path
+UI_FONT = 'path'
+UI_FONT_SIZE = 18
+
+# general colors
+WATER_COLOR = '#71ddee'
+UI_BG_COLOR = '#222222'
+UI_BORDER_COLOR = '#111111'
+TEXT_COLOR = '#EEEEEE'
+
+# ui colors
+HEALTH_COLOR = 'red'
+ENERGY_COLOR = 'blue'
+UI_BORDER_COLOR_ACTIVE = 'gold'
+
 # weapons
 weapon_data = {
-    'sword': {'cooldown': 100, 'damage': 10, 'graphic': 'sprites/weapons/sword/sword.png'},
-    'axe': {'cooldown': 200, 'damage': 25, 'graphic': 'sprites/weapons/axe/axe.png'},
+    'sword': {'cooldown': 100, 'damage': 10, 'graphic': 'sprites/weapons/sword/full.png'},
+    'axe': {'cooldown': 200, 'damage': 25, 'graphic': 'sprites/weapons/axe/full.png'},
 }
